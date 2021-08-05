306271203f519c6b30fbd3b90a67e1aaadb1776e
Crowdbotics <team@crowdbotics.com>
2021-08-05 16:45:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lively_bonus_29518Identifier",
+  url: "https://lively_bonus_29518Identifier.botics.co",
+  api: "https://lively_bonus_29518Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
